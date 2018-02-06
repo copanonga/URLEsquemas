d4617346e143e014c7a9ef5b728957b6ad338d60
José Manuel Suárez Gutiérrez <jose.suarez@tippingpoint.es>
2018-02-06 10:04:15 +0100

    Añadido URL types con URL Schemes a Info.plist. Añadido método para recoger el valor del esquema.

--- URLEsquemas/AppDelegate.m
+++ URLEsquemas/AppDelegate.m
@@ -47,5 +47,11 @@
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+	
+	NSLog (@"URL del esquema: %@", url);
+	
+	return YES;
+}
 
 @end
